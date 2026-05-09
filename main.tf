1806ee8821abd297a42fc4d9695c3130336581d5
Leen Alroomi <alroomileen@gmail.com>
2026-05-09 01:24:18 +0000

    update terraform project

--- main.tf
+++ main.tf
@@ -1,15 +1,39 @@
+# ENABLE REQUIRED API
+resource "google_project_service" "service_networking" {
+  service = "servicenetworking.googleapis.com"
+}
+
+resource "google_project_service" "sql_admin" {
+  service = "sqladmin.googleapis.com"
+}
+
 #VPC
 resource "google_compute_network" "vpc" {
   name                    = "secure-vpc"
   auto_create_subnetworks = false
 }
 
-#SUBNETWORK
+#PRIVATE IP ALLOCATION
+resource "google_compute_global_address" "private_ip_alloc" {
+  name          = "private-ip-alloc"
+  purpose       = "VPC_PEERING"
+  address_type  = "INTERNAL"
+  prefix_length = 16
+  network       = google_compute_network.vpc.self_link
+}
+
+resource "google_service_networking_connection" "private_vpc_connection" {
+  network                 = google_compute_network.vpc.self_link
+  service                 = "servicenetworking.googleapis.com"
+  reserved_peering_ranges = [google_compute_global_address.private_ip_alloc.name]
+}
+
+#SUBNET
 resource "google_compute_subnetwork" "private_subnet" {
   name          = "private-subnet"
   ip_cidr_range = "10.0.1.0/24"
-  region        = "us-central1"
-  network       = google_compute_network.vpc.id
+  region        = var.region
+  network       = google_compute_network.vpc.self_link
   private_ip_google_access = true
 }
 
@@ -23,14 +47,14 @@ resource "google_secret_manager_secret" "db_password" {
 }
 resource "google_secret_manager_secret_version" "db_password_version" {
   secret      = google_secret_manager_secret.db_password.id
-  secret_data = "MySecurePassword123"
+  secret_data = var.db_password
 }
 
 #VM
 resource "google_compute_instance" "vm" {
   name         = "vm"
   machine_type = "e2-micro"
-  zone         = "us-central1-a"
+  zone         = var.zone
 
   boot_disk {
     initialize_params {
@@ -47,14 +71,16 @@ resource "google_compute_instance" "vm" {
 resource "google_sql_database_instance" "db" {
   name             = "secure-db"
   database_version = "MYSQL_8_0"
-  region           = "us-central1"
+  region           = var.region
+  
+  depends_on = [google_service_networking_connection.private_vpc_connection]
 
   settings {
     tier = "db-f1-micro"
 
     ip_configuration {
       ipv4_enabled    = false
-      private_network = google_compute_network.vpc.id
+      private_network = google_compute_network.vpc.self_link
     }
   }
 }
---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "project_id" {
+  type = string
+}
+
+variable "region" {
+  default = "us-central1"
+}
+
+variable "zone" {
+  default = "us-central1-a"
+}
+
+variable "db_password" {
+  sensitive = true
+}
